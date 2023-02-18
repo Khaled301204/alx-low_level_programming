2a309ae28f1966bcecfeadc0fc004bc17d89ff68
khaled301204 <khaledharfoosh2014@gmail.com>
2023-02-17 17:26:00 -0800

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 9 ; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
+	printf("9")
 	putchar('\n');
 	return (0);
 }
